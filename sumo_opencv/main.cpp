768221ebe7703b7db096c8b379b243487b7f6791
Simon Helling <simon.helling@gmx.de>
2018-09-11 16:50:58 +0200

    -classes and stuff?

--- sumo_opencv/main.cpp
+++ sumo_opencv/main.cpp
@@ -7,15 +7,66 @@ Date   : 17.02.2017
 #include "config.h"
 #include "arucoFunc.h"
 #include "detect.h"
-
+#include <boost/thread/thread.hpp>
 
 /* NAMESPACES */
 using namespace std;
 using namespace cv;
 
+class ContinuousCapture 
+{
+	VideoCapture inputVideo_;
+public:
+	cv::Mat image_;
+	bool videoIsOpened_ = false;
+
+	ContinuousCapture() {
+		videoIsOpened_ = inputVideo_.open(0);
+	}
+	ContinuousCapture(int cameraIndex) {
+		videoIsOpened_ = inputVideo_.open(cameraIndex);
+	}
+	ContinuousCapture(double FrameWidth, double FrameHeight) {
+		videoIsOpened_ = inputVideo_.open(0);
+		inputVideo_.set(CAP_PROP_FRAME_HEIGHT, FrameHeight);
+		inputVideo_.set(CAP_PROP_FRAME_WIDTH, FrameWidth);
+	}
+	ContinuousCapture(int cameraIndex, double FrameWidth, double FrameHeight) {
+		videoIsOpened_ = inputVideo_.open(cameraIndex);
+		inputVideo_.set(CAP_PROP_FRAME_HEIGHT, FrameHeight);
+		inputVideo_.set(CAP_PROP_FRAME_WIDTH, FrameWidth);
+	}
+	void capture()
+	{
+		if (videoIsOpened_)
+		{
+			while (inputVideo_.read(image_))
+			{
+
+			}
+		}
+	}
+};
+
+class ArucoDetection 
+{
+	Ptr<aruco::Dictionary> dictionary;
+public:
+	
+};
+
+ContinuousCapture VidCapture(0, 2592, 2048);
+
+
+void detectMarkers2()
+{
+
+}
 
 int main(int argc, char *argv[])
 {
+	boost::thread CaptureThread(VidCapture.capture);
+	boost::thread DetectMarkersThread();
 	int error = ERR_OK;
 #if GENERATE_ARUCO_CODES == SUMO_OPENCV_MODE
 	error = generateAruco();
